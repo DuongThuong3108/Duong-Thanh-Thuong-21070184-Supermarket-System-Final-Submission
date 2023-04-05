edd1243873e03b012c05a4fbe8e2bb820b95ef39
Duong Thuong <Admin@DTTHs-LAPTOP>
2023-04-05 07:57:45 +0700

    Add model validation and association

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+    has_many:stuffs
+    validates_presence_of:title
 end
--- app/models/stuff.rb
+++ app/models/stuff.rb
@@ -1,2 +1,6 @@
 class Stuff < ApplicationRecord
+    belongs_to:genre
+    validates_presence_of:name, :expiry, :manufactured
+    validates_uniqueness_of:name
+    validates_numericality_of:expiry, :manufactured
 end
